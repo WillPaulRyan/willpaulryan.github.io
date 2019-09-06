bd34ea4917212a0340b2435cd2e0fe9b6b457530
WillPaulRyan <willpaulryan@gmail.com>
2019-09-05 22:52:30 -0400

    Updates

---
+++ precache-manifest.c2f10912e6804e190f992d4bccbf2ed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00bc43283a696ed8f7dd4df6f0abb48",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "64d8f32679359f7451a0",
+    "url": "/portfolio/static/css/main.c627ba75.chunk.css"
+  },
+  {
+    "revision": "cc95cf52ff9118fd5293",
+    "url": "/portfolio/static/js/2.51678b07.chunk.js"
+  },
+  {
+    "revision": "64d8f32679359f7451a0",
+    "url": "/portfolio/static/js/main.6d327272.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  }
+]);
