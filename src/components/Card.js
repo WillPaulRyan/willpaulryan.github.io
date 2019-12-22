d9f94b2b135c558ffd8bc6fe6176bbd3da7a392b
WillPaulRyan <willpaulryan@gmail.com>
2019-12-22 14:02:14 +0100

    Make Github links target='_blank'

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 class Card extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       translateX: 0,
@@ -10,18 +10,16 @@ class Card extends React.Component {
       centerX: null,
       centerY: null
     };
-    
+
     this.imgRef = React.createRef();
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
   componentDidMount() {
-    this.setState(
-      {
+    this.setState({
       centerX: this.imgRef.current.clientWidth / 2,
       centerY: this.imgRef.current.clientHeight / 2
-      }
-    );
+    });
   }
 
   handleMouseMove(e) {
@@ -35,16 +33,36 @@ class Card extends React.Component {
 
   render() {
     const bgPosition = {
-      transform: `scale(1.2) translate(${this.state.translateX}, ${this.state.translateY})` 
-    }
+      transform: `scale(1.2) translate(${this.state.translateX}, ${this.state.translateY})`
+    };
 
     return (
-      <div className="project" >
-        <img ref={this.imgRef} style={bgPosition} onMouseMove={this.handleMouseMove} src={this.props.pic} alt={this.props.name} />
+      <div className="project">
+        <img
+          ref={this.imgRef}
+          style={bgPosition}
+          onMouseMove={this.handleMouseMove}
+          src={this.props.pic}
+          alt={this.props.name}
+        />
         <h4 className="name project-text">{this.props.name}</h4>
         <p className="description project-text">{this.props.description}</p>
-        <a href={this.props.url} className="url project-text" target="_blank" rel="noopener noreferrer">Live Site</a>
-        <a href={this.props.github} className="github project-text">Github</a>
+        <a
+          href={this.props.url}
+          className="url project-text"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Live Site
+        </a>
+        <a
+          href={this.props.github}
+          className="github project-text"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
       </div>
     );
   }
