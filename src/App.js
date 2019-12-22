5ae239821fefdb9aac694db832e3cf2104f0bb12
WillPaulRyan <willpaulryan@gmail.com>
2019-12-22 12:15:01 +0100

    Efficiency improvements

--- src/App.js
+++ src/App.js
@@ -1,123 +1,94 @@
-import React from 'react';
-import Page from './components/Page'
-import './App.css';
-import Particles from 'react-particles-js';
-
+import React from "react";
+import Page from "./components/Page";
+import "./App.css";
+import Particles from "react-particles-js";
 
 class App extends React.Component {
   render() {
     return (
       <div id="app-container">
-        <Particles 
-          style={{backgroundColor: '#222'}}
-            params={{
-              "particles": {
-                "number": {
-                  "value": 150,
-                  "density": {
-                    "enable": true,
-                    "value_area": 1803.4120608655228
-                  }
-                },
-                "color": {
-                  "value": "#ffffff"
-                },
-                "shape": {
-                  "type": "circle",
-                  "stroke": {
-                    "width": 0,
-                    "color": "#ffffff"
-                  },
-                  "polygon": {
-                    "nb_sides": 5
-                  },
-                },
-                "opacity": {
-                  "value": 0.5,
-                  "random": false,
-                  "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "size": {
-                  "value": 3,
-                  "random": true,
-                  "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "line_linked": {
-                  "enable": true,
-                  "distance": 100,
-                  "color": "#ffffff",
-                  "opacity": 0.4,
-                  "width": 1
-                },
-                "move": {
-                  "enable": true,
-                  "speed": 5,
-                  "direction": "none",
-                  "random": false,
-                  "straight": false,
-                  "out_mode": "out",
-                  "bounce": false,
-                  "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                  }
+        <Particles
+          style={{ backgroundColor: "#222" }}
+          params={{
+            particles: {
+              number: {
+                value: 150,
+                density: {
+                  enable: true,
+                  value_area: 1803.4120608655228
                 }
               },
-              "interactivity": {
-                "detect_on": "canvas",
-                "events": {
-                  "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                  },
-                  "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                  },
-                  "resize": true
-                },
-                "modes": {
-                  "grab": {
-                    "distance": 500,
-                    "line_linked": {
-                      "opacity": 1
-                    }
-                  },
-                  "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                  },
-                  "repulse": {
-                    "distance": 100,
-                    "duration": 0.4
-                  },
-                  "push": {
-                    "particles_nb": 3
-                  },
-                  "remove": {
-                    "particles_nb": 2
-                  }
+              color: {
+                value: "#ffffff"
+              },
+              shape: {
+                type: "circle"
+              },
+              opacity: {
+                value: 0.5,
+                random: false
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false
                 }
               },
-              "retina_detect": true
-            }} />
+              line_linked: {
+                enable: true,
+                distance: 100,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1
+              },
+              move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200
+                }
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push"
+                },
+                resize: true
+              },
+              modes: {
+                repulse: {
+                  distance: 100,
+                  duration: 0.4
+                },
+                push: {
+                  particles_nb: 3
+                }
+              }
+            },
+            retina_detect: true
+          }}
+        />
         <Page />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,103 +1,89 @@
-import React from 'react';
-import Buttons from './Buttons';
-import Profile from './Profile';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import './Page.css';
-
-
+import React from "react";
+import Buttons from "./Buttons";
+import Profile from "./Profile";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import "./Page.css";
 
 class Page extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: 'home',
+      display: "home"
     };
   }
 
-  handleClick = (event) => {
+  handleClick = event => {
     if (event.target.id) {
       this.setState({
         display: event.target.id
       });
-    }
-    else if (event.target.parentNode.id) {
+    } else if (event.target.parentNode.id) {
       this.setState({
         display: event.target.parentNode.id
       });
-    }
-    else {
+    } else {
       this.setState({
         display: event.target.parentNode.parentNode.id
       });
     }
+  };
+
+  renderSwitch(display) {
+    switch (display) {
+      case "about":
+        return (
+          <div className="section">
+            <About />
+          </div>
+        );
+      case "projects":
+        return (
+          <div className="section">
+            <Projects />
+          </div>
+        );
+      case "contact":
+        return (
+          <div className="section">
+            <Contact />
+          </div>
+        );
+      default:
+        return null;
+    }
   }
 
   render() {
-    if (this.state.display === 'about') {
-      return (
-        <div id="page">
-          <Buttons handleClick={this.handleClick.bind(this)} active={this.state.display} />
-          <div id="content">
-            <Profile />
-            <div className="section">
-              <About />
-            </div>
-          </div>
+    return (
+      <div id="page">
+        <Buttons
+          handleClick={this.handleClick.bind(this)}
+          active={this.state.display}
+        />
+        <div id="content">
+          <Profile />
+          {this.renderSwitch(this.state.display)}
         </div>
-      )
-    }
-    else if (this.state.display === 'projects') {
-      return (
-        <div id="page">
-          <Buttons handleClick={this.handleClick.bind(this)} active={this.state.display} />
-          <div id="content">
-            <Profile />
-            <div className="section">
-              <Projects />
-            </div>
-          </div>
-        </div>
-      )
-    }
-    else if (this.state.display === 'contact') {
-      return (
-        <div id="page">
-          <Buttons handleClick={this.handleClick.bind(this)} active={this.state.display} />
-          <div id="content">
-            <Profile />
-            <div className="section">
-              <Contact />
-            </div>
-          </div>
-        </div>
-      )
-    }
-    else {
-      return (
-        <div id="page">
-          <Buttons handleClick={this.handleClick.bind(this)} active={this.state.display} />
-          <div id="content">
-            <Profile />
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 
   // Project Image Pre-load
   componentDidMount() {
-    var images = ["/images/tommy_ogp.png", 
-                  "/images/wr3_ogp.png", 
-                  "/images/2020_ogp.png",
-                  "/images/emoji_speller_ogp.png",
-                  "/images/virtual_monkeys_ogp.png",
-                  "/images/flashcards_ogp.png"];
+    const images = [
+      "/images/tommy_ogp.png",
+      "/images/wr3_ogp.png",
+      "/images/2020_ogp.png",
+      "/images/emoji_speller_ogp.png",
+      "/images/virtual_monkeys_ogp.png",
+      "/images/flashcards_ogp.png"
+    ];
     for (let i = 0; i < images.length; i++) {
-      (new Image()).src = images[i];
+      new Image().src = images[i];
     }
-  };
+  }
 }
 
 export default Page;
